d24ea7461f516be0b7f7ce84e7ab81c27d32b8b3
nanthawatpop <82141825+nanthawatpop@users.noreply.github.com>
2021-04-10 12:42:44 +0700

    Update Nanthawat.py

--- Nanthawat.py
+++ Nanthawat.py
@@ -5,3 +5,39 @@
 
 #<strong>Nanthawat</strong>
 
+#<i> I am interested in data science since I would like to apply for master degree in data science.</i>
+
+#<h3> The following equaution illustrate how to multiply first three prime numbers</h3>
+
+#In[5]
+
+no1 = 3
+no2 = 5
+no3 = 7
+
+#multiply all
+sum = no1 * no2 * no3 
+
+#Display the outcome
+print("sum")
+
+
+#<ul>
+# <li>Math</li>
+# <li>Science</li>
+# <li>Art</li>
+#</ul>
+
+#<ol>
+# <li>Math</li>
+# <li>Science</li>
+# <li>Art</li>
+#</ol>
+
+# <a href="url">link text</a>
+
+#In[]:
+
+
+
+
